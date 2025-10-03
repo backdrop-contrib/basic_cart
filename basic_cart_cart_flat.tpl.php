944a54b718c1e5d8d61432a2d714358ef60e5abf
Robert Garrigos <robert@garrigos.cat>
2025-10-03 11:05:19 +0200

    Changes row class by line class

--- basic_cart_cart_flat.tpl.php
+++ basic_cart_cart_flat.tpl.php
@@ -15,7 +15,7 @@ foreach ($cart as $cid => $value) {
 if (is_numeric($cid))
 {
 $node = node_load($cart[$value]);
-      print '<div class="basic-cart-cart-contents row">';
+      print '<div class="basic-cart-cart-contents line">';
       print '<div class="basic-cart-cart-node-title cell">' . l($node->title, 'node/' . $node->nid) . '</div>';
       print '<div class="basic-cart-cart-quantity cell">' . $_SESSION['basic_cart']['cart'][$cid. 'quantity'] . '</div>';
       print '<div class="basic-cart-cart-x cell">x</div>';
@@ -27,14 +27,14 @@ $node = node_load($cart[$value]);
 }
 ?>
    <div class="basic-cart-cart basic-cart-grid">
-      <div class="basic-cart-cart-total-price-contents row">
+      <div class="basic-cart-cart-total-price-contents line">
         <div class="basic-cart-total-price cell">
           <?php print t('Total price'); ?>: <strong> <?php print $price; ?></strong>
         </div>
       </div>
-    
+
       <?php if (!empty ($vat)): ?>
-        <div class="basic-cart-cart-total-vat-contents row">
+        <div class="basic-cart-cart-total-vat-contents line">
           <div class="basic-cart-total-vat cell"><?php print t('Total VAT'); ?>: <strong><?php print $vat; ?></strong></div>
         </div>
       <?php endif; ?>
--- basic_cart_cart_render_block.tpl.php
+++ basic_cart_cart_render_block.tpl.php
@@ -15,7 +15,7 @@ foreach ($cart as $cid => $value) {
 if (is_numeric($cid))
 {
 $node = node_load($cart[$value]);
-      print '<div class="basic-cart-cart-contents row">';
+      print '<div class="basic-cart-cart-contents line">';
       print '<div class="basic-cart-cart-node-title cell">' . l($node->title, 'node/' . $node->nid) . '</div>';
       print '<div class="basic-cart-cart-quantity cell">' . $_SESSION['basic_cart']['cart'][$cid. 'quantity'] . '</div>';
       print '<div class="basic-cart-cart-x cell">x</div>';
@@ -26,17 +26,17 @@ $node = node_load($cart[$value]);
 }
 }
 ?>
-      <div class="basic-cart-cart-total-price-contents row">
+      <div class="basic-cart-cart-total-price-contents line">
         <div class="basic-cart-total-price cell">
           <?php print t('Total price'); ?>:<strong> <?php print $price; ?></strong>
         </div>
       </div>
       <?php if (!empty ($vat)): ?>
-        <div class="basic-cart-block-total-vat-contents row">
+        <div class="basic-cart-block-total-vat-contents line">
           <div class="basic-cart-total-vat cell"><?php print t('Total VAT'); ?>: <strong><?php print $vat; ?></strong></div>
         </div>
       <?php endif; ?>
-      <div class="basic-cart-cart-checkout-button basic-cart-cart-checkout-button-block row">
+      <div class="basic-cart-cart-checkout-button basic-cart-cart-checkout-button-block line">
         <?php print l(t('View cart'), 'cart', array('attributes' => array('class' => array('button')))); ?>
       </div>
     <?php endif; ?>
