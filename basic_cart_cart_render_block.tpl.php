04cf51820ba6269f2a8f7268ce4baed6c395288b
Robert Garrigos <robert@garrigos.cat>
2025-12-20 07:01:58 +0100

    fixes #40: use stripe currency settings

--- basic_cart_cart_render_block.tpl.php
+++ basic_cart_cart_render_block.tpl.php
@@ -5,33 +5,32 @@
  */
 ?>
 
-<?php if (empty($cart)): ?>
+<?php if (empty($cart)) : ?>
   <p><?php print t('Your cart is empty.'); ?></p>
-<?php else: ?>
+<?php else : ?>
   <div class="basic-cart-grid basic-cart-block">
-<?php if(is_array($cart) && count($cart) >= 1): ?>
-      <?php
-foreach ($cart as $cid => $value) {
-if (is_numeric($cid))
-{
-$node = node_load($cart[$value]);
-      print '<div class="basic-cart-cart-contents line">';
-      print '<div class="basic-cart-cart-node-title cell">' . l($node->title, 'node/' . $node->nid) . '</div>';
-      print '<div class="basic-cart-cart-quantity cell">' . $_SESSION['basic_cart']['cart'][$cid. 'quantity'] . '</div>';
-      print '<div class="basic-cart-cart-x cell">x</div>';
-      print '<div class="basic-cart-cart-unit-price cell">';
-      print '<strong>' . basic_cart_price_format($node->price["und"][0]["value"]) . '</strong>';
-      print '</div>';
-      print '</div>';
-}
-}
-?>
+  <?php if (is_array($cart) && count($cart) >= 1) : ?>
+    <?php
+    foreach ($cart as $cid => $value) {
+      if (is_numeric($cid)) {
+        $node = node_load($cart[$value]);
+          print '<div class="basic-cart-cart-contents line">';
+          print '<div class="basic-cart-cart-node-title cell">' . l($node->title, 'node/' . $node->nid) . '</div>';
+          print '<div class="basic-cart-cart-quantity cell">' . $_SESSION['basic_cart']['cart'][$cid. 'quantity'] . '</div>';
+          print '<div class="basic-cart-cart-x cell">x</div>';
+          print '<div class="basic-cart-cart-unit-price cell">';
+          print '<strong>' . basic_cart_price_format($node->price["und"][0]["value"]) . '</strong>';
+          print '</div>';
+          print '</div>';
+      }
+    }
+    ?>
       <div class="basic-cart-cart-total-price-contents line">
         <div class="basic-cart-total-price cell">
           <?php print t('Total price'); ?>:<strong> <?php print $price; ?></strong>
         </div>
       </div>
-      <?php if (!empty ($vat)): ?>
+      <?php if (!empty ($vat)) : ?>
         <div class="basic-cart-block-total-vat-contents line">
           <div class="basic-cart-total-vat cell"><?php print t('Total VAT'); ?>: <strong><?php print $vat; ?></strong></div>
         </div>
@@ -39,6 +38,6 @@ $node = node_load($cart[$value]);
       <div class="basic-cart-cart-checkout-button basic-cart-cart-checkout-button-block line">
         <?php print l(t('View cart'), 'cart', array('attributes' => array('class' => array('button')))); ?>
       </div>
-    <?php endif; ?>
+  <?php endif; ?>
   </div>
 <?php endif; ?>
